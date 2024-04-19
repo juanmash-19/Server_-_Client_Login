136e7d8d1e736c22eaa4dc89e2f205796519bf33
JuanMartin <juanmasuhe1909@gmail.com>
2024-04-19 18:03:25 -0500

    Arreglando el login final

--- client-project/src/App.js
+++ client-project/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Register } from './components/auth/Register';
+import Login from './components/auth/Login'; 
 
 function App() {
   return (
-    <Register />
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
--- client-project/src/components/auth/Login.jsx
+++ client-project/src/components/auth/Login.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { User } from "../../request/User"; 
+import TextField from '@mui/material/TextField';
+import VisibilityOff from '@mui/icons-material/VisibilityOff'; 
+import Button from '@mui/material/Button';
+import OutlinedInput from '@mui/material/OutlinedInput';
+
+import { useNavigate } from 'react-router-dom';
+
+const userController = new User();
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+    const [showPassword, setShowPassword] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
-export default Login
+    const handleLogin = async () => {
+        const data = {
+            email,
+            password,
+        }
+        try {
+            const response = await userController.login(data); 
+            console.log(response);
+            if (response.status === 200) {
+                alert("Autenticación exitosa");
+                navigate('/dashboard'); 
+            } else {
+                alert("Error en el usuario o contraseña ");
+            }
+        } catch (error) {
+            console.error("Error en la solicitud:", error);
+        }
+    };
+
+    return (
+        <div className='card-login'>
+            <TextField 
+                id="standard-basic" 
+                label="Email" 
+                variant="standard" 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type='email' 
+                fullWidth
+            />
+
+            <OutlinedInput
+                id="password"
+                type={showPassword ? 'text' : 'password'}
+                onChange={(e) => setPassword(e.target.value)}
+                endAdornment={
+                    <VisibilityOff onClick={() => setShowPassword(!showPassword)} />
+                }
+                label="Password"
+                value={password}
+            />
+
+            <Button onClick={handleLogin} variant="contained">Iniciar sesión</Button>
+        </div>
+    );
+};
+
+export default Login;
